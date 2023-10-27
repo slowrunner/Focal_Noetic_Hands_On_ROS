1e76174c5237698b41e73f5095182d2b73873882
slowrunner <slowrunner@users.noreply.github.com>
2023-10-27 15:05:27 -0400

    add copy convenience scripts to workspace

--- setup/get_noetic_HoROS_code.sh
+++ setup/get_noetic_HoROS_code.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cp -r ~/Focal_Noetic_Hands_On_ROS/noetic-HoROS-code/ ~/Hands-On-ROS-for-Robotics-Programming
+# Copy book version of chapter code, with ROS Noetic version for Chap3 and gopigo3_node 
+cp -r ~/Focal_Noetic_Hands_On_ROS/noetic-HoROS-code/ ~/Hands-On-ROS-for-Robotics-Programming  
+# Copy some convenience scripts to the workspace  
+cp -r ~/Focal_Noetic_Hands_On_ROS/catkin_ws_in_git/* ~/catkin_ws  
 
