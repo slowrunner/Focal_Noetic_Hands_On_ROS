7d4b82cc3a39cf200365cbed2815168c28828814
slowrunner <slowrunner@users.noreply.github.com>
2023-10-26 22:22:08 -0400

    rather than cloned version, get my changed for Python3 versions

--- catkin_ws_in_git/get_gopigo.sh
+++ catkin_ws_in_git/get_gopigo.sh
@@ -2,7 +2,6 @@
 
 echo "GET Basic GoPiGo for start of CHAPTER 6 (pg 177, ebook page 192)"
 cd ~/catkin_ws/src
-# git clone https://github.com/ros-gopigo/gopigo3_node
-cp -r ~/Focal_Noetic_Hands_On_ROS/noetic-HoROS-code/gopigo3_node ~/catkin_ws/src
+git clone https://github.com/ros-gopigo/gopigo3_node
 cd ~/catkin_ws
 catkin_make
