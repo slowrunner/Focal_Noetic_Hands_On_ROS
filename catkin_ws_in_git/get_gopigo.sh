e3a9214ad5fb6e37e87bcce63961dfc067061a8a
slowrunner <slowrunner@users.noreply.github.com>
2023-10-28 15:33:58 -0400

    get noetic version

--- catkin_ws_in_git/get_gopigo.sh
+++ catkin_ws_in_git/get_gopigo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "GET Basic GoPiGo for start of CHAPTER 6 (pg 177, ebook page 192)"
-cd ~/catkin_ws/src
-git clone https://github.com/ros-gopigo/gopigo3_node
+echo "GET Noetic version of Basic GoPiGo for start of CHAPTER 6 (pg 177, ebook page 192)"
+# cd ~/catkin_ws/src
+# git clone https://github.com/ros-gopigo/gopigo3_node
+cp -R ~/Hands-On-ROS-for-Robotics-Programming/gopigo3_node ~/catkin_ws/src
 cd ~/catkin_ws
 catkin_make
+source ss.sh
+echo "gopigo3_node ready to run with: run_gopigo3.sh"
