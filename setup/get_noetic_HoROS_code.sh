73c8ede60fe93a6435b0b9f26d4963470d8aa1e2
slowrunner <slowrunner@users.noreply.github.com>
2023-10-28 23:54:13 -0400

    forgot to make run_gopigo3.sh executable

--- setup/get_noetic_HoROS_code.sh
+++ setup/get_noetic_HoROS_code.sh
@@ -3,5 +3,7 @@
 # Copy book version of chapter code, with ROS Noetic version for Chap3 and gopigo3_node 
 cp -r ~/Focal_Noetic_Hands_On_ROS/noetic-HoROS-code/ ~/Hands-On-ROS-for-Robotics-Programming  
 # Copy some convenience scripts to the workspace  
-cp -r ~/Focal_Noetic_Hands_On_ROS/catkin_ws_in_git/* ~/catkin_ws  
+cp -r ~/Focal_Noetic_Hands_On_ROS/catkin_ws_in_git/* ~/catkin_ws
+# forgot to make executable
+chmod +x ~/catkin_ws/run_gopigo3.sh
 
