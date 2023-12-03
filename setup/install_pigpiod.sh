d7f6867ab1dce9dce75d8a2e1b7f4dfe0effccb7
slowrunner <slowrunner@users.noreply.github.com>
2023-12-03 11:31:08 -0500

    correct path to service file

--- setup/install_pigpiod.sh
+++ setup/install_pigpiod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo systemctl stop pigpiod.service
-sudo cp /home/ubuntu/utils/pigpiod.service /etc/systemd/system
+sudo cp /home/pi/Focal_Noetic_Hands_On_ROS/setup/pigpiod.service /etc/systemd/system
 sudo systemctl enable pigpiod.service
 sudo systemctl start pigpiod.service
 systemctl status pigpiod.service
